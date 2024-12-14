fa0370ea8f7f20d738316c08fc571d342fa02ea0
willotavio <89710130+willotavio@users.noreply.github.com>
2024-12-14 14:18:42 -0300

    feat: add Derivator and PBKDF2Derivator classes

---
+++ services/derivator/derivator.py
@@ -0,0 +1,5 @@
+from abc import ABC, abstractmethod
+
+class Derivator(ABC):
+    def derive(self, *args, **kwargs):
+        pass
---
+++ services/derivator/pbkdf2_derivator.py
@@ -0,0 +1,28 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.hashes import SHA256
+
+from .derivator import Derivator
+
+class PBKDF2Derivator(Derivator):
+    def __init__(self):
+        self.backend = default_backend()
+
+    def derive(self, password: str, salt: bytes, key_length: int, iv_length: int = 16):
+        iterations = 10000
+        
+        pbkdf2 = PBKDF2HMAC(
+            algorithm=SHA256(),
+            salt=salt,
+            length=key_length + iv_length,
+            iterations=iterations,
+            backend=self.backend
+        )
+
+        password_bytes = password.encode("utf-8")
+        key_and_iv = pbkdf2.derive(password_bytes)
+
+        return {
+            "key": key_and_iv[:key_length],
+            "iv": key_and_iv[key_length:]
+        }
