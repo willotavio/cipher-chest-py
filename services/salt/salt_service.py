2d5bbced8dabd17d7799fdf8952fbf171e1aacef
willotavio <89710130+willotavio@users.noreply.github.com>
2024-12-14 15:41:59 -0300

    feat: add error handling in SaltService

--- services/salt/salt_service.py
+++ services/salt/salt_service.py
@@ -1,8 +1,12 @@
 import secrets
+import logging
 
 class SaltService:
     def generate_salt(self, bytes_size: int = 32):
-        salt = bytearray(bytes_size)
-        for i in range(bytes_size):
-            salt[i] = secrets.randbelow(256)
-        return bytes(salt)
+        try:
+            salt = bytearray(bytes_size)
+            for i in range(bytes_size):
+                salt[i] = secrets.randbelow(256)
+            return bytes(salt)
+        except (ValueError, TypeError) as e :
+            logging.exception(f"Invalid argument: {e}")
