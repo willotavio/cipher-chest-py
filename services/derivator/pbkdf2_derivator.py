27d6d431215d48adbd96d0113a79ae43e8588083
willotavio <89710130+willotavio@users.noreply.github.com>
2024-12-14 15:47:30 -0300

    feat: add error handling in PBKDF2Derivator

--- services/derivator/pbkdf2_derivator.py
+++ services/derivator/pbkdf2_derivator.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.hashes import SHA256
+import logging
 
 from .derivator import Derivator
 
@@ -9,20 +10,25 @@ class PBKDF2Derivator(Derivator):
         self.backend = default_backend()
 
     def derive(self, password: str, salt: bytes, key_length: int, iv_length: int = 16):
-        iterations = 10000
-        
-        pbkdf2 = PBKDF2HMAC(
-            algorithm=SHA256(),
-            salt=salt,
-            length=key_length + iv_length,
-            iterations=iterations,
-            backend=self.backend
-        )
+        try:
+            iterations = 10000
+            
+            pbkdf2 = PBKDF2HMAC(
+                algorithm=SHA256(),
+                salt=salt,
+                length=key_length + iv_length,
+                iterations=iterations,
+                backend=self.backend
+            )
 
-        password_bytes = password.encode("utf-8")
-        key_and_iv = pbkdf2.derive(password_bytes)
+            password_bytes = password.encode("utf-8")
+            key_and_iv = pbkdf2.derive(password_bytes)
 
-        return {
-            "key": key_and_iv[:key_length],
-            "iv": key_and_iv[key_length:]
-        }
+            return {
+                "key": key_and_iv[:key_length],
+                "iv": key_and_iv[key_length:]
+            }
+        except (ValueError, TypeError) as e:
+            logging.exception(f"Invalid arguments: {e}")
+        except AttributeError as e:
+            logging.exception(f"Attribute error: {e}")
