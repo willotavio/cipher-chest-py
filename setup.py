eb45854b9ba579b1ed95aebadcbb7e103bd714e6
willotavio <89710130+willotavio@users.noreply.github.com>
2024-12-15 10:35:06 -0300

    chore: update setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="enc_py",
+    name="cipher_chest",
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "altgraph==0.17.4",
-        "cffi==1.16.0",
-        "cryptography==42.0.5",
-        "packaging==24.1",
-        "pefile==2023.2.7",
-        "pycparser==2.22",
-        "pyinstaller==6.9.0",
-        "pyinstaller-hooks-contrib==2024.7",
-        "pywin32-ctypes==0.2.2"
+        "cffi==1.17.1",
+        "cryptography==44.0.0",
+        "pycparser==2.22"
     ]
 )
